a0815ce48f6231f4d634f181501fa67f7112a92e
Raghav Anandan <raghav@Raghavs-MacBook-Pro.local>
2018-03-15 16:32:35 -0700

    DB change commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://admin:onlyadmin@ds215089.mlab.com:15089/healthcamp_spa');
+// mongoose.connect('mongodb://admin:onlyadmin@ds215089.mlab.com:15089/healthcamp_spa');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
